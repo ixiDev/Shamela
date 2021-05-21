66753d9a6e45f992f02dea08ea7f79fd0c82ce4f
ixi <feedback.mrzero@gmail.com>
2021-05-21 22:49:50 +0100

    - Create tests for BokFileReader.kt

---
+++ common/bokfilereader/src/test/java/ixidev/bokfilereader/BokFileReaderTest.kt
@@ -0,0 +1,48 @@
+package ixidev.bokfilereader
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class BokFileReaderTest(
+    private val bookId: Int
+) {
+
+    lateinit var reader: BokFileReader
+
+    @Before
+    fun readFile() {
+        reader = BokFileReader("src/test/assets/$bookId.bok")
+    }
+
+    @Test
+    fun readMainTable() {
+        val bokFile = reader.getBokFile()
+        assert(bokFile.main.BkId != null)
+        assertEquals(bokFile.main.BkId, bookId)
+    }
+
+    @Test
+    fun readBTable() {
+        val bokFile = reader.getBokFile()
+        val bElements = bokFile.bElements
+        assert(bElements.isNotEmpty())
+    }
+
+    @Test
+    fun readTTable() {
+        val bokFile = reader.getBokFile()
+        val tElements = bokFile.tElements
+        assert(tElements.isNotEmpty())
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{index}: {0}")
+        fun createTestData() = arrayOf(151045, 151100, 151007)
+    }
+
+}
