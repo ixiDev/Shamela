58cd71917cce22a32aceeed1193581ec68bf2e28
ixi <feedback.mrzero@gmail.com>
2021-05-21 21:22:36 +0100

    - Add MainTable.kt

---
+++ common/bokfilereader/src/main/java/ixidev/bokfilereader/tables/MainTable.kt
@@ -0,0 +1,47 @@
+package ixidev.bokfilereader.tables
+
+
+data class MainTable(
+    /**
+     * Id
+     */
+    val BkId: Int? = null,
+    /**
+     * Title
+     */
+    val Bk: String? = null,
+    /**
+     * Release info [عن الكتاب]
+     */
+    val Betaka: String? = null,
+    val Inf: String? = null,
+    /**
+     * Author name
+     */
+    val Auth: String? = null,
+    /**
+     * Author full bio
+     */
+    val AuthInf: String? = null,
+    val TafseerNam: String? = null,
+    val IslamShort: Int? = null,
+    val oNum: Int? = null,
+    val oVer: Int? = null,
+    val seal: String? = null,
+    val oAuth: Int? = null,
+    val bVer: Int? = null,
+    val Pdf: Int? = null,
+    val oAuthVer: Int? = null,
+    val verName: String? = null,
+    val cat: String? = null,
+    val Lng: String? = null,
+    /**
+     * Release date in Higri
+     */
+    val HigriD: String? = null,
+    val AD: Int? = null,
+    val aSeal: String? = null,
+    val bLnk: String? = null,
+    val PdfCs: Int? = null,
+    val ShrtCs: Int? = null
+)
