5d2c8cd59e428dc0f3375c63fdea838c20735750
ixi <feedback.mrzero@gmail.com>
2021-05-24 18:07:46 +0100

    - Add BookPage.kt - Add BookSummary.kt

---
+++ common/data/src/main/java/ixidev/shamela/data/entities/BookPage.kt
@@ -0,0 +1,12 @@
+package ixidev.shamela.data.entities
+
+/**
+ * Created by Abdelmajid ID ALI, on 24/05/2021
+ * Github : [https://github.com/ixiDev]
+ */
+data class BookPage(
+    val id: Int,
+    val page: Int,
+    val part: Int,
+    val content: String
+)
---
+++ common/data/src/main/java/ixidev/shamela/data/entities/BookSummary.kt
@@ -0,0 +1,12 @@
+package ixidev.shamela.data.entities
+
+/**
+ * Created by Abdelmajid ID ALI, on 24/05/2021
+ * Github : [https://github.com/ixiDev]
+ */
+data class BookSummary(
+    val id: Int,
+    val page: Int,
+    val parent: Int,
+    val title: String
+)
